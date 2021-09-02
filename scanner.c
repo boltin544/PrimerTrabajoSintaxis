a46c29e9c3d4379043b7df32a51704512962e259
Tomas Boltin <tomas.boltin@hotmail.com>
2021-09-02 14:55:22 -0300

    primer commit

---
+++ scanner.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "scanner.h"
+
+int main() {
+   
+    enum tokens { FDT, SEP, CAD, SPA };
+    const char* tokens[3] = {"Fin De Texto:", "Separador", "Cadena"};
+    char caracter; 
+    int tipoCaracter;
+    
+    caracter = getchar();
+    
+    while(caracter != EOF) 
+    {
+        if(!isspace(caracter)) 
+        {
+           if(caracter == ','){
+                
+               tipoCaracter = SEP;
+               printf(tokens[tipoCaracter]);
+               printf(": %c", caracter ," \t\n");   
+            }
+           
+           else if(caracter == EOF){
+               tipoCaracter = FDT;
+           }
+           else{
+               tipoCaracter = CAD;
+               printf(tokens[tipoCaracter]);
+               printf(": %c", caracter , " \t\n");
+           
+           }
+        }
+    
+            printf("\n");
+    
+        caracter = getchar();
+    } 
+
+    printf(tokens[FDT]);
+     
+   return 0;
+}
